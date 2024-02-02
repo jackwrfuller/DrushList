718d0bf9f1bea6af67488e36f9cc092b596fbc6a
jackwrfuller <jack.fuller101@gmail.com>
2024-02-02 17:09:54 +1100

    add ability to list entity types

--- src/Drush/EntityListCommands.php
+++ src/Drush/EntityListCommands.php
@@ -8,7 +8,7 @@ use Consolidation\OutputFormatters\StructuredData\RowsOfFields;
 use Drupal\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Entity\EntityFieldManager;
 use Drush\Attributes as CLI;
-
+use Symfony\Component\Console\Helper\Table;
 
 
 final class EntityListCommands extends AbstractListCommands
@@ -27,8 +27,23 @@ final class EntityListCommands extends AbstractListCommands
 
     #[CLI\Command(name: 'entity:list')]
     #[CLI\Argument(name: 'entityType', description: 'entity type to display')]
-    public function entityList(string $entityType): void
+    public function entityList(string $entityType = null): void
     {
+        if ($entityType === null)
+        {
+            $table = new Table($this->output());
+            $table->setStyle('compact');
+            // Gets all entities
+            $entityTypes = array_keys(\Drupal::entityTypeManager()->getDefinitions());
+            // Gets all fieldable entities, which are the only ones that work to list fields
+            $entityTypes = array_keys(\Drupal::service('entity_field.manager')->getFieldMap());
+            foreach ($entityTypes as $i => $type) {
+                $table->addRow([$i => $type]);
+            }
+            $table->render();
+        }
+
+
         // Get required metadata about the requested entity type
         $entityTypeClass = \Drupal::entityTypeManager()->getStorage($entityType)->getEntityClass();
         $entityTypeID = \Drupal::entityTypeManager()->getStorage($entityType)->getEntityType()->getKey('id');
